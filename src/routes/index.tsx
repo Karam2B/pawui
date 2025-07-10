db320bd68eb2834f5665c17fbcc341fc78b7610c
Karam Barakat <kb.karam.barakat@gmail.com>
2025-07-10 14:12:56 -0700

    init

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,25 +1,31 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useStylesScoped$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 
 export default component$(() => {
-  return (
-    <>
-      <h1>Hi 👋</h1>
-      <div>
-        Can't wait to see what you build with qwik!
-        <br />
-        Happy coding.
-      </div>
-    </>
-  );
+    useStylesScoped$(`
+        .bar > div { padding: 0 0.5em }
+
+    `);
+
+    return (
+        <div class="flex flex-col h-screen">
+            <div class="root flex-1">
+                hello world
+            </div>
+            <h1 class="bar bg-slate-800 text-white flex">
+                <div class="bg-green-500 text-black">COMMAND</div>
+                <div>/</div>
+            </h1>
+        </div>
+    );
 });
 
 export const head: DocumentHead = {
-  title: "Welcome to Qwik",
-  meta: [
-    {
-      name: "description",
-      content: "Qwik site description",
-    },
-  ],
+    title: "Welcome to Qwik",
+    meta: [
+        {
+            name: "description",
+            content: "Qwik site description",
+        },
+    ],
 };
